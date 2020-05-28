3f011d3ae71effcf1947cdafabd0d0cd70c16498
Ruochen Ji <jiruoc@gmail.com>
2020-05-28 16:58:52 +0800

    launch staircase

---
+++ java/improvise/src/staircase.java
@@ -0,0 +1,16 @@
+public class staircase {
+    String buff = "h";
+    public staircase() {
+
+    }
+
+    static void interesting() {
+        System.out.print("?");
+    }
+
+    public static void main(String[] args) {
+        staircase foo = new staircase();
+        interesting();
+        System.out.print(foo.buff);
+    }
+}

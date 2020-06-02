c39721bb8c78b9e90132ed7abb3036a9ea3ee5fa
Ruochen Ji <jiruoc@gmail.com>
2020-06-02 16:54:29 +0800

    selection sort comment

--- java/course_princeton_algorithm_java/src/SelectionSort.java
+++ java/course_princeton_algorithm_java/src/SelectionSort.java
@@ -2,11 +2,18 @@ import java.util.Comparator;
 
 public class SelectionSort<T> {
 
+    int numberOfCompare;
+
+    public SelectionSort() {
+        numberOfCompare = 0;
+    }
+
     public void sort(T[] list, Comparator<T> comparator) {
         for (int i = 0; i < list.length - 1; i++) {
             int smallest = i;
             for (int j = i + 1; j < list.length; j++) {
                 if (comparator.compare(list[j], list[smallest]) <= 0) smallest = j;
+                numberOfCompare++;
             }
             swap(list, i, smallest);
         }
@@ -19,11 +26,12 @@ public class SelectionSort<T> {
     }
 
     public static void main(String[] args) {
-        Integer[] test = {2, 6, 9, 10, 2, 3, 20, 68, 1, 43, 20};
+        Integer[] test = {2, 6, 9, 10, 2, 3, 20, 68, 1, 43, 20, 30};
         int value = 7;
         for (int i : test) {
             System.out.print(i + ",");
         }
+        System.out.println();
         SelectionSort<Integer> Sort = new SelectionSort<Integer>();
         Sort.sort(test, new Comparator<Integer>() {
             @Override
@@ -31,7 +39,7 @@ public class SelectionSort<T> {
                 return o1 - o2;
             }
         });
-        System.out.println();
+        System.out.println(Sort.numberOfCompare); // ~ o(n^2): still too slow
         for (int i : test) {
             System.out.print(i + ",");
         }

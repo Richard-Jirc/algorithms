391a8b180741b72e493d3601489b67372d808f37
Ruochen Ji <jiruoc@gmail.com>
2020-06-02 16:45:22 +0800

    selection sort bug fix

--- java/course_princeton_algorithm_java/src/SelectionSort.java
+++ java/course_princeton_algorithm_java/src/SelectionSort.java
@@ -6,7 +6,7 @@ public class SelectionSort<T> {
         for (int i = 0; i < list.length - 1; i++) {
             int smallest = i;
             for (int j = i + 1; j < list.length; j++) {
-                if (comparator.compare(list[j], list[i]) < 0) smallest = j;
+                if (comparator.compare(list[j], list[smallest]) <= 0) smallest = j;
             }
             swap(list, i, smallest);
         }
@@ -19,7 +19,7 @@ public class SelectionSort<T> {
     }
 
     public static void main(String[] args) {
-        Integer[] test = {2, 6, 9, 10, 2};
+        Integer[] test = {2, 6, 9, 10, 2, 3, 20, 68, 1, 43, 20};
         int value = 7;
         for (int i : test) {
             System.out.print(i + ",");
@@ -28,7 +28,7 @@ public class SelectionSort<T> {
         Sort.sort(test, new Comparator<Integer>() {
             @Override
             public int compare(Integer o1, Integer o2) {
-                return o2 - o1;
+                return o1 - o2;
             }
         });
         System.out.println();

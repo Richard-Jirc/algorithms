717efedc3218a773559a4501aed98b76401d6b08
Ruochen Ji <jiruoc@gmail.com>
2020-06-07 17:00:08 +0800

    board solver 6.7

--- java/course_princeton_algorithm_java/src/Solver.java
+++ java/course_princeton_algorithm_java/src/Solver.java
@@ -1,7 +1,47 @@
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Iterator;
+
 public class Solver {
+    MinPQ<SearchNode> queue;
+    private final boolean solvable;
+    private static class SearchNode implements Comparable<SearchNode> {
+        Board board;
+        int moves, manhattan, priority;
+        public SearchNode(Board step) {
+            board = step;
+            manhattan = step.manhattan();
+        }
+        @Override
+        public int compareTo(SearchNode node) {
+            return this.priority - node.priority;
+        }
+    }
 
-    public Solver() {
+    public Solver(Board initial) {
+        if (initial == null) throw new IllegalArgumentException();
+        solvable = this.isSolvable();
+        queue = new MinPQ<>();
+    }
+    public boolean isSolvable() {
+        return true;
+    }
 
+    public int moves() {
+        if (!solvable) return -1;
+
+
+        return 0;
+    }
+    public Iterable<Board> solution() {
+        if (!solvable) return null;
+
+        return new Iterable<Board>() {
+            @Override
+            public Iterator<Board> iterator() {
+                return null;
+            }
+        };
     }
 
     public static void main(String[] args) {

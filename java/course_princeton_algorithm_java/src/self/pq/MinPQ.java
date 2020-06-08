9857fdccde7f47775fdcec94e71614e8f4a86154
Ruochen Ji <jiruoc@gmail.com>
2020-06-08 20:29:19 +0800

    MinPQ 6.8 3

--- java/course_princeton_algorithm_java/src/self/pq/MinPQ.java
+++ java/course_princeton_algorithm_java/src/self/pq/MinPQ.java
@@ -3,12 +3,20 @@ package self.pq;
 public class MinPQ<T extends Comparable<T>> {
 
     private T[] list;
+    private int size;
 
     public MinPQ() {
         list = (T[]) new Object[7];
     }
     public void insert(T item) {
+        list[size] = item;
 
+    }
+    public int size() {
+        return size;
+    }
+    public boolean isEmpty() {
+        return size == 0;
     }
     private void sink(T item) {
 

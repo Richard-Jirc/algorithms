c23566dbd22db7adf3641b25596f7fa27f7ad5a1
Ruochen Ji <jiruoc@gmail.com>
2020-06-19 16:05:22 +0800

    balanced search tree 6.19 2
    pretty annoying...

--- java/course_princeton_algorithm_java/src/self/symbolTable/TwoThreeTree.java
+++ java/course_princeton_algorithm_java/src/self/symbolTable/TwoThreeTree.java
@@ -2,20 +2,39 @@ package self.symbolTable;
 
 public class TwoThreeTree<Key extends Comparable<Key>, Value> {
     private class Node {
-        Key key;
-        Value value;
+        Key key1, key2;
+        Value value1, value2;
         boolean dual = true;
         Node left, mid, right;
-        public Node(Key k, Value v) {
-            key = k;
-            value = v;
+        public Node(Key key, Value value) {
+            key1 = key;
+            value1 = value;
         }
         public String toString() {
             return "!";
         }
+        public boolean isDual() {
+            boolean DUAL = true;
+            return dual == DUAL;
+        }
+        public void insertTwo(Key key, Value value) {
+            assert dual;
+            if (key1.compareTo(key) == 0) value1 = value;
+            else {
+                if (key1.compareTo(key) < 0) {
+                    key2 = key;
+                    value2 = value;
+                } else {
+                    key2 = key1;
+                    value2 = value1;
+                    key1 = key;
+                    value1 = value;
+                }
+                dual = false;
+            }
+        }
     }
-    private final boolean DUAL = true;
-    private final boolean TRI = false;
+
     private Node root;
 
     /** Overloaded Constructors */
@@ -23,8 +42,28 @@ public class TwoThreeTree<Key extends Comparable<Key>, Value> {
     public TwoThreeTree(Key key, Value value) {
         root = new Node(key, value);
     }
-    private boolean isDual(Node node) {
-        return node.dual == DUAL;
+
+
+    public Value search(Key key) {
+        Node node = root;
+        while (node != null) {
+            if (node.isDual()) {
+                if (key.compareTo(node.key1) < 0) node = node.left;
+                if (key.compareTo(node.key1) > 0) node = node.right;
+                else return node.value1;
+            } else {
+                if (key.compareTo(node.key1) < 0) node = node.left;
+                if (key.compareTo(node.key1) == 0) return node.value1;
+                if (key.compareTo(node.key2) < 0) node = node.mid;
+                if (key.compareTo(node.key2) == 0) return node.value2;
+                if (key.compareTo(node.key2) > 0) node = node.right;
+            }
+        }
+        return null;
+    }
+
+    public void insert(Key key, Value value) {
+        Node node = root;
     }
 
     public static void main(String[] args) {

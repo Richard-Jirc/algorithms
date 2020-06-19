3642a32600db423dbda2136ac7ed7cb1809f94ab
Ruochen Ji <jiruoc@gmail.com>
2020-06-19 11:08:02 +0800

    BST 6.19
    binary-search-tree self-implementation.
    obviously, without proper balancing, the tree will be constructed in a way that operations can't be ~ log(n)

--- java/course_princeton_algorithm_java/src/self/symbolTable/BinarySearchTree.java
+++ java/course_princeton_algorithm_java/src/self/symbolTable/BinarySearchTree.java
@@ -1,16 +1,33 @@
 package self.symbolTable;
 
 import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
 
 public class BinarySearchTree<Key extends Comparable<Key>, Value> {
-    Node root;
+    private Node root;
     private class Node {
         Key key;
         Value value;
         Node left;
         Node right;
-        public Node() {
-
+        int count;
+        public Node(Key k, Value v) {
+            key = k;
+            value = v;
+        }
+        public String toString() {
+            StringBuilder string = new StringBuilder();
+            if (left != null) string.append(left.key);
+            else string.append("-");
+            string.append(" <= ");
+            string.append("K:");
+            string.append(key);
+            string.append(" V:");
+            string.append(value);
+            string.append(" => ");
+            if (right != null) string.append(right.key);
+            else string.append("-");
+            return string.toString();
         }
     }
     public BinarySearchTree() {
@@ -19,38 +36,72 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         return root == null;
     }
 
-    /**GET method
+    /**GET method:
      * perform binary search for a certain key.
      * @return Value if found, null if not exist.
      */
     public Value get(Key key) {
-        Node result = search(root, key);
-        if (result != null) return result.value;
-        else return null;
+        Node node = root;
+        while (node != null) {
+            StdOut.println(node.toString());
+            if (node.key.compareTo(key) == 0) return node.value;
+            if (node.key.compareTo(key) < 0) node = node.right;
+            else node = node.left;
+        }
+        return null;
     }
-    /**GET method HELPER
+
+    /**PUT method:
+     * perform binary search,
+     * if no matching key found then create a new one,
+     * update if found.
      */
-    private Node search(Node node, Key key) {
-        if (node == null) return null;
-        if (node.key.compareTo(key) == 0) return node;
-        if (node.key.compareTo(key) < 0) return search(node.left, key);
-        else return search(node.right, key);
-    }
-
     public void put(Key key, Value value) {
-
-    }
-    private void allocate(Node node, Key key, Value value) {
-
+        Node node = root;
+        Node newNode = new Node(key, value);
+        while (node != null) {
+            if (node.key.compareTo(key) == 0) node = newNode;
+            if (node.key.compareTo(key) > 0) {
+                if (node.left == null) {
+                    node.left = newNode;
+                    break;
+                }
+                node = node.left;
+            } else {
+                if (node.right == null) {
+                    node.right = newNode;
+                    break;
+                }
+                node = node.right;
+            }
+        }
+        if (this.isEmpty()) root = newNode;
     }
 
     public Iterable<Value> iterable() {
         Queue<Value> queue = new Queue<>();
-        
+        collect(queue, root);
+        return queue;
+    }
+    private void collect(Queue<Value> q, Node node) {
+        if (node == null) return;
+        collect(q, node.left);
+        q.enqueue(node.value);
+        collect(q, node.right);
     }
 
 
     public static void main(String[] args) {
+        BinarySearchTree<Integer, String> test = new BinarySearchTree<>();
+        test.put(5, "5!");
+        test.put(10, "10!");
+        test.put(2, "2!");
+        test.put(6, "6!");
+        Iterable<String> peek = test.iterable();
+        for (String i : peek) {
+            System.out.println(i);
+        }
+        System.out.println(test.get(6));
 
     }
 }

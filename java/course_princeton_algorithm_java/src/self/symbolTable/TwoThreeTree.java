9188e3761a31a0398cfb1727ea5b1ebc963beb61
Ruochen Ji <jiruoc@gmail.com>
2020-06-28 11:31:47 +0800

    balanced search tree 6.28
    not really much progress

--- java/course_princeton_algorithm_java/src/self/symbolTable/TwoThreeTree.java
+++ java/course_princeton_algorithm_java/src/self/symbolTable/TwoThreeTree.java
@@ -2,61 +2,63 @@ package self.symbolTable;
 
 public class TwoThreeTree<Key extends Comparable<Key>, Value> {
     private class Node {
-        Key key1, key2;
-        Value value1, value2;
+        Key leftKey, rightKey;
+        Value leftVal, rightVal;
         boolean dual = true;
         Node left, mid, right;
         public Node(Key key, Value value) {
-            key1 = key;
-            value1 = value;
+            leftKey = key;
+            leftVal = value;
         }
-        public String toString() {
-            return "!";
-        }
+
         public boolean isDual() {
             boolean DUAL = true;
             return dual == DUAL;
         }
         public void insertTwo(Key key, Value value) {
             assert dual;
-            if (key1.compareTo(key) == 0) value1 = value;
+            if (leftKey.compareTo(key) == 0) leftVal = value;
             else {
-                if (key1.compareTo(key) < 0) {
-                    key2 = key;
-                    value2 = value;
+                if (leftKey.compareTo(key) < 0) {
+                    rightKey = key;
+                    rightVal = value;
                 } else {
-                    key2 = key1;
-                    value2 = value1;
-                    key1 = key;
-                    value1 = value;
+                    rightKey = leftKey;
+                    rightVal = leftVal;
+                    leftKey = key;
+                    leftVal = value;
                 }
                 dual = false;
             }
+        }
+        public String toString() {
+            return "!";
         }
     }
 
     private Node root;
 
     /** Overloaded Constructors */
-    public TwoThreeTree() {}
     public TwoThreeTree(Key key, Value value) {
         root = new Node(key, value);
     }
-
+    public boolean isEmpty() {
+        return root == null;
+    }
 
     public Value search(Key key) {
         Node node = root;
         while (node != null) {
             if (node.isDual()) {
-                if (key.compareTo(node.key1) < 0) node = node.left;
-                if (key.compareTo(node.key1) > 0) node = node.right;
-                else return node.value1;
+                if (key.compareTo(node.leftKey) < 0) node = node.left;
+                if (key.compareTo(node.leftKey) > 0) node = node.right;
+                else return node.leftVal;
             } else {
-                if (key.compareTo(node.key1) < 0) node = node.left;
-                if (key.compareTo(node.key1) == 0) return node.value1;
-                if (key.compareTo(node.key2) < 0) node = node.mid;
-                if (key.compareTo(node.key2) == 0) return node.value2;
-                if (key.compareTo(node.key2) > 0) node = node.right;
+                if (key.compareTo(node.leftKey) < 0) node = node.left;
+                if (key.compareTo(node.leftKey) == 0) return node.leftVal;
+                if (key.compareTo(node.rightKey) < 0) node = node.mid;
+                if (key.compareTo(node.rightKey) == 0) return node.rightVal;
+                if (key.compareTo(node.rightKey) > 0) node = node.right;
             }
         }
         return null;
@@ -64,6 +66,11 @@ public class TwoThreeTree<Key extends Comparable<Key>, Value> {
 
     public void insert(Key key, Value value) {
         Node node = root;
+
+        if (node.isDual()) node.insertTwo(key, value);
+        else {
+            
+        }
     }
 
     public static void main(String[] args) {

9365575e4d1f56070eb94f91394ae662d9e62770
Ruochen Ji <jiruoc@gmail.com>
2020-06-29 15:49:12 +0800

    KdTree 6.29

--- java/course_princeton_algorithm_java/src/KdTree.java
+++ java/course_princeton_algorithm_java/src/KdTree.java
@@ -1,7 +1,14 @@
-public class KdTree {
+public class KdTree<K extends Comparable<K>, V> {
+    private class Node {
+        K key;
+        V value;
+        private int depth;
+        public Node() {
+
+        }
+    }
 
     public KdTree() {
-        
     }
 
     public static void main(String[] args) {

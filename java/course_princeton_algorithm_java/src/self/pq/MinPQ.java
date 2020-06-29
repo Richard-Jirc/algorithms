cbd6ce6c85d65355399a79d0d857d2b77b4e5f86
Ruochen Ji <jiruoc@gmail.com>
2020-06-29 18:34:16 +0800

    MinPQ 6.29
    worked out so slowly. my brain does not feel right -_-?

--- java/course_princeton_algorithm_java/src/self/pq/MinPQ.java
+++ java/course_princeton_algorithm_java/src/self/pq/MinPQ.java
@@ -1,19 +1,25 @@
 package self.pq;
 import edu.princeton.cs.algs4.StdDraw;
 
+/** MinPQ:
+ *  keep the SMALLEST item at the root of the binary heap.
+ * */
 public class MinPQ<T extends Comparable<T>> {
 
     private final T[] list;
     private int size;
 
+
     //  -,"1","2",   "4",            "8",         ...
     // [0, 1, 2,3, 4,5,6,7, 8,9,10,11,12,13,14,15 ...]
     public MinPQ() {
         list = (T[]) new Comparable[8];
+        size = 1;
     }
     public void insert(T item) {
         list[size] = item;
         swim(size);
+        size++;
     }
     public int size() {
         return size;
@@ -21,18 +27,16 @@ public class MinPQ<T extends Comparable<T>> {
     public boolean isEmpty() {
         return size == 0;
     }
-    private void sink(int index) {
-        while (index < size) {
-            index *= 2;
-            if (list[index].compareTo(list[index + 1]) < 0) index++;
-            
-        }
+
+    /**LESS helper function:
+     * if true, a < b.
+     */
+    private boolean less(int a, int b) {
+        return list[a].compareTo(list[b]) <= 0;
     }
-    private void swim(int index) {
-        while (list[index / 2].compareTo(list[index]) >= 0 && index > 1) {
-            swap(index, index / 2);
-            index /= 2;
-        }
+    private int min(int a, int b) {
+        if (less(a, b)) return a;
+        else return b;
     }
     private void swap(int a, int b) {
         T mid = list[b];
@@ -40,8 +44,27 @@ public class MinPQ<T extends Comparable<T>> {
         list[a] = mid;
     }
 
+    /**SINK helper function: if any child is smaller than self, recursively swap. */
+    private void sink(int index) {
+        while (index * 2 + 1 < size) {
+            int smaller = min(index * 2, index * 2 + 1);
+            if (less(index, smaller)) break;
+            swap(smaller, index);
+            index = smaller;
+        }
+    }
+
+    /**SWIM helper function: if self is smaller than parent, recursively swap.*/
+    private void swim(int index) {
+        while (less(index, index / 2) && index > 1) {
+            swap(index, index / 2);
+            index /= 2;
+        }
+    }
+
+
+
     public static void main(String[] args) {
-        StdDraw.setCanvasSize(800, 600);
-        StdDraw.point(1, 1);
+
     }
 }

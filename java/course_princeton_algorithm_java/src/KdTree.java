b0ca5a209eeb2a2ea43218200f5b5652f7d9a77e
Ruochen Ji <jiruoc@gmail.com>
2020-07-02 16:06:54 +0800

    Kd Tree 7.2 3
    draw() function

--- java/course_princeton_algorithm_java/src/KdTree.java
+++ java/course_princeton_algorithm_java/src/KdTree.java
@@ -1,6 +1,7 @@
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.Queue;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
 
 public class KdTree {
     private static class Node {
@@ -8,13 +9,20 @@ public class KdTree {
         private RectHV rect;
         private Node left, right;
         private final boolean x;
-        public Node(Point2D p, boolean useX) {
+        public Node(Point2D p, boolean useX, RectHV area) {
             pt = p;
             x = useX;
-            if (useX) rect = new RectHV(0, 0, p.x(), 1);
-            else rect = new RectHV()
+            rect = area;
         }
 
+        public RectHV leftRect() {
+            if (x) return new RectHV(rect.xmin(), rect.ymin(), pt.x(), rect.ymax());
+            return new RectHV(rect.xmin(), rect.ymin(), rect.xmax(), pt.y());
+        }
+        public RectHV rightRect() {
+            if (x) return new RectHV(pt.x(), rect.ymin(), rect.xmax(), rect.ymax());
+            return new RectHV(rect.xmin(), pt.y(), rect.xmax(), rect.ymax());
+        }
         /**COMPARE {@code Node} with a {@code Point2D}
          * @param p to compare with
          * @return {@code int > 0} if search should go LEFT.
@@ -22,10 +30,6 @@ public class KdTree {
         public double compare(Point2D p) {
             if (x) return this.pt.x() - p.x();
             else return this.pt.y() - p.y();
-        }
-
-        public boolean worthSearch(Point2D target) {
-            return true;
         }
         public String toString() {
             StringBuilder string = new StringBuilder();
@@ -54,20 +58,17 @@ public class KdTree {
      * @param p to insert
      */
     public void insert(Point2D p) {
-        root = put(root, p, true);
+        RectHV canvas = new RectHV(0, 0, 1, 1);
+        root = put(root, p, true, canvas);
     }
-    private Node put(Node x, Point2D p, boolean useX) {
+    private Node put(Node x, Point2D p, boolean useX, RectHV rect) {
         if (x == null) {
             size++;
-            return new Node(p, useX);
+            return new Node(p, useX, rect);
         }
         double cmp = x.compare(p);
-        if (cmp > 0) x.left = put(x.left, p, !useX);
-        else if (cmp < 0) x.right = put(x.right, p, !useX);
-        else {
-            if (!x.pt.equals(p)) x.right = put(x.right, p, !useX);
-            else x.pt = p;
-        }
+        if (cmp > 0) x.left = put(x.left, p, !useX, x.leftRect());
+        else if (cmp < 0 || !x.pt.equals(p)) x.right = put(x.right, p, !useX, x.rightRect());
         return x;
     }
 
@@ -89,7 +90,6 @@ public class KdTree {
     }
 
     /**RECTANGLE SEARCH
-     *
      * @param rect to search
      * @return a {@code Queue} object as {@code Iterable};
      */
@@ -98,7 +98,9 @@ public class KdTree {
 
         return list;
     }
-    private void search()
+    private void search() {
+
+    }
 
     /**NEAREST POINT SEARCH
      * @param p as target point
@@ -112,16 +114,32 @@ public class KdTree {
 //
 //    }
 
+    public void draw() {
+        if (root != null) draw(root);
+    }
+    private void draw(Node node) {
+        StdDraw.setPenRadius(0.002);
+        if (node.x) {
+            StdDraw.setPenColor(StdDraw.RED); // draw vertical line!
+            StdDraw.line(node.pt.x(), node.rect.ymin(), node.pt.x(), node.rect.ymax());
+        } else {
+            StdDraw.setPenColor(StdDraw.BLUE); // draw horizontal line!
+            StdDraw.line(node.rect.xmin(), node.pt.y(), node.rect.xmax(), node.pt.y());
+        }
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.filledCircle(node.pt.x(), node.pt.y(), 0.005);
+        if (node.left != null) draw(node.left);
+        if (node.right != null) draw(node.right);
+    }
+
     /**DETERMINE if new entry point is a CLOSER ONE.
      * @param target supplied target Point
      * @param p first Pt
      * @param q second Pt
-     * @return {@code true} if q is CLOSER, equal returns {@code false}.
+     * @return {@code true} if q is CLOSER, equal or bigger returns {@code false}.
      */
     private boolean closer(Point2D target, Point2D p, Point2D q) {
-        double pDis = Math.pow(target.x() - p.x(), 2) + Math.pow(target.y() - p.y(), 2);
-        double qDis = Math.pow(target.x() - q.x(), 2) + Math.pow(target.y() - q.y(), 2);
-        return qDis < pDis;
+        return target.distanceSquaredTo(q) < target.distanceSquaredTo(p);
     }
     public static void main(String[] args) {
         KdTree test = new KdTree();

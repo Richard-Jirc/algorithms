fddd786e76437c657c856d6dc87263e2f54aba40
Ruochen Ji <jiruoc@gmail.com>
2020-07-18 21:27:47 +0800

    WordNet init

---
+++ java/course_princeton_algorithm_java/src/SAP.java
@@ -0,0 +1,15 @@
+/* AlgorithmII-CHAPTER 1 ASSIGNMENT: SAP */
+
+import edu.princeton.cs.algs4.Digraph;
+
+public class SAP {
+    public SAP(Digraph G) {
+        if (G == null) throw new IllegalArgumentException("constructor argument null");
+        
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- java/course_princeton_algorithm_java/src/WordNet.java
+++ java/course_princeton_algorithm_java/src/WordNet.java
@@ -1,8 +1,15 @@
 /* AlgorithmII-CHAPTER 1 ASSIGNMENT: WordNet */
 
+import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.RedBlackBST;
+
 public class WordNet {
 
+    RedBlackBST<Integer, Bag<Integer>> connection;
+
     public WordNet(String synsets, String hypernyms) {
+        if (synsets == null || hypernyms == null) throw new IllegalArgumentException("constructor argument null");
+        connection = new RedBlackBST<>();
 
     }
 

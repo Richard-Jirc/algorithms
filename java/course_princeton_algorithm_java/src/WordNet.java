c3d7069ee522aadf0abe94d8b5892b43190afe4a
Ruochen Ji <jiruoc@gmail.com>
2020-07-21 09:04:14 +0800

    WordNet 7.21

--- java/course_princeton_algorithm_java/src/WordNet.java
+++ java/course_princeton_algorithm_java/src/WordNet.java
@@ -2,17 +2,18 @@
 
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.RedBlackBST;
+import edu.princeton.cs.algs4.StdIn;
 
 public class WordNet {
 
-    RedBlackBST<Integer, String> synsetsTree;
+    RedBlackBST<Integer, String> word;
     RedBlackBST<Integer, Bag<Integer>> connection;
 
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null) throw new IllegalArgumentException("constructor argument null");
-        synsetsTree = new RedBlackBST<>();
+        word = new RedBlackBST<>();
         connection = new RedBlackBST<>();
-
+        StdIn.hasNextLine()
     }
     
     public boolean isNoun(String word) {

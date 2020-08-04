88a056b564e350fc347f1610d7b7bd82374d3668
Ruochen Ji <jiruoc@gmail.com>
2020-08-04 17:32:34 +0800

    SAP 8.4

---
+++ java/course_princeton_algorithm_java/src/DeluxeBFS.java
@@ -0,0 +1,49 @@
+/* AlgorithmII-CHAPTER 1 ASSIGNMENT
+ helper class DeluxeBFS */
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Queue;
+
+public class DeluxeBFS {
+
+    private final int[] edgeTo, distTo;
+    private final boolean[] marked;
+
+    public DeluxeBFS(Digraph G, int s) {
+        edgeTo = new int[G.V()];
+        distTo = new int[G.V()];
+        marked = new boolean[G.V()];
+        bfs(G, s);
+    }
+
+    public boolean access(int v) {
+        return marked[v];
+    }
+    public int shortestDis(int v) {
+        int j = v;
+        while (j != edgeTo[j]) {
+            j = edgeTo[j];
+        }
+        return distTo[v];
+    }
+
+    private void bfs(Digraph G, int s) {
+        Queue<Integer> q = new Queue<>();
+        q.enqueue(s);
+        marked[s] = true;
+        distTo[s] = 0;
+        int depth = 0;
+        while (!q.isEmpty()) {
+            int pop = q.dequeue();
+            Iterable<Integer> adj = G.adj(pop);
+            depth++;
+            for (int i : adj) {
+                if (marked[i]) continue;
+                edgeTo[i] = pop;
+                distTo[i] = depth;
+                q.enqueue(i);
+            }
+        }
+    }
+
+}
--- java/course_princeton_algorithm_java/src/SAP.java
+++ java/course_princeton_algorithm_java/src/SAP.java
@@ -2,19 +2,22 @@
 
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
 
 import java.util.Arrays;
 
 public class SAP {
 
     private final Digraph graph; // the digraph that need to be processed
-    private int[] edgeTo, distTo;
+    private final int[] edgeToV, distToV, edgeToW, distToW; // BFS search arrays
 
     public SAP(Digraph G) {
         if (G == null) throw new IllegalArgumentException("constructor argument null");
         graph = G;
-        edgeTo = new int[G.V()];
-        distTo = new int[G.V()];
+        edgeToV = new int[G.V()];
+        distToV = new int[G.V()];
+        edgeToW = new int[G.V()];
+        distToW = new int[G.V()];
     }
 
     /**
@@ -22,19 +25,48 @@ public class SAP {
      * @param v first
      * @param w second
      * @return {@code length} of shortest ancestral path, {@code -1} when the path do not exist.
+     * @throws IllegalArgumentException when index out of range
      */
     public int length(int v, int w) {
         checkV(v, w);
-        initializeArray(edgeTo); // initialize helper arrays.
-        initializeArray(distTo);
-        
+        // if (v == w) return 0;
+        Arrays.fill(edgeToV, -1); // initialize helper arrays.
+        Arrays.fill(distToV, 0);
+        Arrays.fill(edgeToW, -1);
+        Arrays.fill(distToW, 0);
+        Queue<Integer> fromV = new Queue<>();
+        Queue<Integer> fromW = new Queue<>();
 
+        fromV.enqueue(v);
+        fromW.enqueue(w);
+        int depth = 1;
+        while (!fromV.isEmpty() && !fromW.isEmpty()) {
+            int x = fromV.dequeue();
+            int y = fromW.dequeue();
+            Iterable<Integer> adjX = graph.adj(x);
+            Iterable<Integer> adjY = graph.adj(y);
+            for (int i : adjX) {
+                if (edgeToV[i] != -1) continue;
+                edgeToV[i] = x;
+                distToV[i] = depth;
+                for (int k : adjY) {
+                    if (i == k) {
+                        // found something
+                        return 0;
+                    }
+                    if (edgeToW[k] != -1) continue;
+                    edgeToW[k] = y;
+                    distToW[k] = depth;
+                    fromW.enqueue(k);
+                }
+                fromV.enqueue(i);
+            }
 
-        return 0;
+        }
+        // when the algorithm reaches here it means no common ancestor is found.
+        return -1;
     }
-    /** Override version of above.
-     * @return find the shortest in any combination between {@code v & w}.
-     */
+    /** {@code SET} version of above */
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
         return 0;
     }
@@ -58,11 +90,6 @@ public class SAP {
     /** helper function to check v&w arguments */
     private void checkV(int v, int w) {
         if (v < 0 || v >= graph.V() || w < 0 || w >= graph.V()) throw new IllegalArgumentException("vertex out of range");
-    }
-
-    /** wash every element to {@code -1} */
-    private void initializeArray(int[] array) {
-        Arrays.fill(array, -1);
     }
 
 

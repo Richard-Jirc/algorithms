aac9bf083a83da293c3f3cec8ab918cd1418f56e
Ruochen Ji <jiruoc@gmail.com>
2020-08-04 11:27:25 +0800

    WordNet 8.4

--- java/course_princeton_algorithm_java/src/SAP.java
+++ java/course_princeton_algorithm_java/src/SAP.java
@@ -1,15 +1,53 @@
 /* AlgorithmII-CHAPTER 1 ASSIGNMENT: SAP */
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
 
 public class SAP {
+
+    Digraph graph;
+
     public SAP(Digraph G) {
         if (G == null) throw new IllegalArgumentException("constructor argument null");
-
+        
     }
 
+    /**
+     * find the shortest ancestral path and return its length.
+     * @param v first
+     * @param w second
+     * @return {@code length} of shortest ancestral path, {@code -1} when the path do not exist.
+     */
+    public int length(int v, int w) {
+        if (v < 0 || v > )
+        return 0;
+    }
+    /** Override version of above.
+     * @return find the shortest in any combination between {@code v & w}.
+     */
+    public int length(Iterable<Integer> v, Iterable<Integer> w) {
+
+        return 0;
+    }
+
+    /**
+     * find the common ancestor sitting in the shortest ancestral path.
+     * @return {@code index} of common ancestor, {@code -1} if do not exist.
+     */
+    public int ancestor(int v, int w) {
+
+        return 1;
+    }
+    /** Override version of above.
+     * @return find the shortest anyway.
+     */
+    public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
+        return 0;
+    }
 
     public static void main(String[] args) {
-
+        In in = new In(args[0]);
+        Digraph test = new Digraph(in);
+        System.out.print(test.toString());
     }
 }
--- java/course_princeton_algorithm_java/src/WordNet.java
+++ java/course_princeton_algorithm_java/src/WordNet.java
@@ -1,26 +1,36 @@
 /* AlgorithmII-CHAPTER 1 ASSIGNMENT: WordNet */
 
 import edu.princeton.cs.algs4.Bag;
+import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.RedBlackBST;
 import edu.princeton.cs.algs4.StdIn;
 
 public class WordNet {
 
-    RedBlackBST<Integer, String> word;
-    RedBlackBST<Integer, Bag<Integer>> connection;
+    RedBlackBST<String, Integer> word; //
+    Digraph connection;
 
     public WordNet(String synsets, String hypernyms) {
         if (synsets == null || hypernyms == null) throw new IllegalArgumentException("constructor argument null");
         word = new RedBlackBST<>();
-        connection = new RedBlackBST<>();
-        StdIn.hasNextLine();
+        connection = new Digraph(5);
     }
     
-    public boolean isNoun(String word) {
-        if (word == null) throw new IllegalArgumentException("isNoun null");
-        return false;
+    public boolean isNoun(String noun) {
+        if (noun == null) throw new IllegalArgumentException("isNoun null");
+        return word.contains(noun);
+    }
+
+    /**
+     * {@code synset} as common ancestor of {@code nounA & nounB}
+     * in shortest ancestral path.
+     * @return {@code String of synset}
+     */
+    public String sap(String nounA, String nounB) {
+
+        return "test";
     }
     public static void main(String[] args) {
-
+        WordNet test = new WordNet(args[0], args[1]);
     }
 }
